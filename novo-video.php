0df08f047397aa438cedaf1576d0190faff5bf1f
dawsompablo <dawsompablo@gmail.com>
2023-05-08 19:43:37 -0300

    refac: changes of directories

--- novo-video.php
+++ novo-video.php
@@ -5,12 +5,12 @@ $pdo = new PDO("sqlite:$dbPath");
 
 $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
 if ($url === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
     exit;
 }
 $title = filter_input(INPUT_POST, 'titulo');
 if ($title === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
     exit;
 }
 
@@ -21,7 +21,7 @@ $statement->bindParam(1, $_POST['url']);
 $statement->bindParam(2, $_POST['titulo']);
 
 if ($statement->execute() === false) {
-    header('Location: /index.php?sucesso=0');
+    header('Location: /?sucesso=0');
 } else {
-    header('Location: /index.php?sucesso=1');
+    header('Location: /?sucesso=1');
 }
